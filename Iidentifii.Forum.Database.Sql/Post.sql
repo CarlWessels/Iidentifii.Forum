97c8c243d136e051f0c8378a5dd9b1fac7201965
Carl Wessels <carl.wessels@gmail.com>
2024-06-08 16:39:51 +0200

    simple database

---
+++ Iidentifii.Forum.Database.Sql/Post.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Post]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[SubformumId] INT NOT NULL,
+	[Title] NVARCHAR(255) NOT NULL,
+	[Content] NVARCHAR(4000) -- MAX
+
+
+	CONSTRAINT [PK_Post] PRIMARY KEY (Id)
+	, CONSTRAINT FK_Post_Subforum FOREIGN KEY ([SubformumId]) REFERENCES Subforum (Id)
+)
